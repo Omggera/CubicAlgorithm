92fe63b8325786aa54a44af601ceae291b96b067
Omggera <omggera@gmail.com>
2022-05-20 15:13:58 +0300

    Добавьте файлы проекта.

---
+++ CubicAlgorithm/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CubicAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] chars = new char[] { 'A', 'B', 'C' };
+            for (int i = 0; i < chars.Length; i++)
+            {
+                for (int j = 0; j < chars.Length; j++)
+                {
+                    for (int k = 0; k < chars.Length; k++)
+                    {
+                        Console.WriteLine($"{chars[i]}{chars[j]}{chars[k]}");
+                    }
+                }
+            }
+        }
+    }
+}
